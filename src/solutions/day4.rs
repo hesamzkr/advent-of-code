afd54b577fdee3854b6a83e093305cbcfbd493b2
hesamzkr <hesam.zk.81@gmail.com>
2022-12-04 18:16:32 +0100

    day4 improved

--- src/solutions/day4.rs
+++ src/solutions/day4.rs
@@ -5,54 +5,46 @@ use crate::common::Solution;
 pub struct Day4;
 
 impl Solution for Day4 {
-    type Parsed = Vec<Vec<String>>;
+    type Parsed = Vec<Vec<u32>>;
     type PartOneOutput = u32;
     type PartTwoOutput = u32;
 
     fn parse(input: String) -> Self::Parsed {
-        input.lines().map(|x| x.split(",").map(|y| y.to_string()).collect()).collect()
+        input
+            .lines()
+            .map(|pair| {
+                pair.split(&['-', ','])
+                    .map(|x| x.parse().unwrap())
+                    .collect()
+            })
+            .collect()
     }
 
     fn part_one(data: &mut Self::Parsed) -> Self::PartOneOutput {
-        let mut total = 0;
-        for pair in data {
-            let x: Vec<u32> = pair[0].split("-").map(|y| y.parse().unwrap()).collect();
-            let y: Vec<u32> = pair[1].split("-").map(|y| y.parse().unwrap()).collect();
-
-            let first: HashSet<u32> = HashSet::from_iter((x[0]..=x[1]));
-            let second: HashSet<u32> = HashSet::from_iter((y[0]..=y[1]));
-
+        data.iter().fold(0, |acc, pair| {
+            let first: HashSet<u32> = HashSet::from_iter(pair[0]..=pair[1]);
+            let second: HashSet<u32> = HashSet::from_iter(pair[2]..=pair[3]);
             let c = first.intersection(&second).count();
 
             if c == first.len() || c == second.len() {
-                total += 1;
+                acc + 1
+            } else {
+                acc
             }
-        }
-
-
-
-        total
+        })
     }
 
     fn part_two(data: &mut Self::Parsed) -> Self::PartTwoOutput {
-        let mut total = 0;
-        for pair in data {
-            let x: Vec<u32> = pair[0].split("-").map(|y| y.parse().unwrap()).collect();
-            let y: Vec<u32> = pair[1].split("-").map(|y| y.parse().unwrap()).collect();
-
-            let first: HashSet<u32> = HashSet::from_iter((x[0]..=x[1]));
-            let second: HashSet<u32> = HashSet::from_iter((y[0]..=y[1]));
-
+        data.iter().fold(0, |acc, pair| {
+            let first: HashSet<u32> = HashSet::from_iter(pair[0]..=pair[1]);
+            let second: HashSet<u32> = HashSet::from_iter(pair[2]..=pair[3]);
             let c = first.intersection(&second).count();
 
             if c > 0 {
-                total += 1;
+                acc + 1
+            } else {
+                acc
             }
-        }
-        total
+        })
     }
-}
-
-impl Day4 {
-    fn util(data: <Day4 as Solution>::Parsed) {}
 }
