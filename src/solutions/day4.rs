676f627ac2b9fe4f1ba4680022b1bf3fa7592f65
hesamzkr <hesam.zk.81@gmail.com>
2022-12-04 06:34:45 +0100

    initial day4 solution

--- src/solutions/day4.rs
+++ src/solutions/day4.rs
@@ -1,22 +1,55 @@
+use std::collections::HashSet;
+
 use crate::common::Solution;
 
 pub struct Day4;
 
 impl Solution for Day4 {
-    type Parsed = Vec<String>;
+    type Parsed = Vec<Vec<String>>;
     type PartOneOutput = u32;
     type PartTwoOutput = u32;
 
     fn parse(input: String) -> Self::Parsed {
-        todo!()
+        input.lines().map(|x| x.split(",").map(|y| y.to_string()).collect()).collect()
     }
 
     fn part_one(data: &mut Self::Parsed) -> Self::PartOneOutput {
-        todo!()
+        let mut total = 0;
+        for pair in data {
+            let x: Vec<u32> = pair[0].split("-").map(|y| y.parse().unwrap()).collect();
+            let y: Vec<u32> = pair[1].split("-").map(|y| y.parse().unwrap()).collect();
+
+            let first: HashSet<u32> = HashSet::from_iter((x[0]..=x[1]));
+            let second: HashSet<u32> = HashSet::from_iter((y[0]..=y[1]));
+
+            let c = first.intersection(&second).count();
+
+            if c == first.len() || c == second.len() {
+                total += 1;
+            }
+        }
+
+
+
+        total
     }
 
     fn part_two(data: &mut Self::Parsed) -> Self::PartTwoOutput {
-        todo!()
+        let mut total = 0;
+        for pair in data {
+            let x: Vec<u32> = pair[0].split("-").map(|y| y.parse().unwrap()).collect();
+            let y: Vec<u32> = pair[1].split("-").map(|y| y.parse().unwrap()).collect();
+
+            let first: HashSet<u32> = HashSet::from_iter((x[0]..=x[1]));
+            let second: HashSet<u32> = HashSet::from_iter((y[0]..=y[1]));
+
+            let c = first.intersection(&second).count();
+
+            if c > 0 {
+                total += 1;
+            }
+        }
+        total
     }
 }
 
