ca5a7af58fa39d6aac9447fecd47b2d935756f87
hesamzkr <hesam.zk.81@gmail.com>
2022-12-06 06:31:46 +0100

    day6 done

--- src/solutions/day6.rs
+++ src/solutions/day6.rs
@@ -1,25 +1,42 @@
+use std::collections::HashSet;
+
 use crate::common::Solution;
 
 pub struct Day6;
 
 impl Solution for Day6 {
-    type Parsed = Vec<String>;
-    type PartOneOutput = u64;
-    type PartTwoOutput = u64;
+    type Parsed = Vec<char>;
+    type PartOneOutput = usize;
+    type PartTwoOutput = usize;
 
     fn parse(input: String) -> Self::Parsed {
-        todo!()
+        input.chars().collect()
     }
 
     fn part_one(data: &mut Self::Parsed) -> Self::PartOneOutput {
-        todo!()
+        for i in 0..data.len() {
+            let mut set: HashSet<char> = HashSet::new();
+            set.insert(data[i]);
+            set.insert(data[i + 1]);
+            set.insert(data[i + 2]);
+            set.insert(data[i + 3]);
+            if set.len() == 4 {
+                return i + 4;
+            }
+        }
+        0
     }
 
     fn part_two(data: &mut Self::Parsed) -> Self::PartTwoOutput {
-        todo!()
+        for i in 0..data.len() {
+            let mut set: HashSet<char> = HashSet::new();
+            for j in 0..14 {
+                set.insert(data[i + j]);
+            }
+            if set.len() == 14 {
+                return i + 14;
+            }
+        }
+        0
     }
-}
-
-impl Day6 {
-    fn util(data: <Day6 as Solution>::Parsed) {}
 }
