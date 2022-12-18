90810a4faccfa49b46cd369f0869e1ce40ed5e4d
hesamzkr <hesam.zk.81@gmail.com>
2022-12-18 17:33:37 +0100

    day 18 done

--- src/solutions/day18.rs
+++ src/solutions/day18.rs
@@ -1,61 +1,9 @@
-use std::collections::HashSet;
-
-#[derive(Debug, PartialEq, Eq, Hash)]
-struct Vector {
-    x: i32,
-    y: i32,
-    z: i32,
-}
-
-impl Vector {
-    fn add(&self, other: &Self) -> Vector {
-        Vector {
-            x: self.x + other.x,
-            y: self.y + other.y,
-            z: self.z + other.z,
-        }
-    }
-
-    fn opposite(&self) -> Vector {
-        Vector {
-            x: -self.x,
-            y: -self.y,
-            z: -self.z,
-        }
-    }
-
-    fn check_surrounding(&self, cubes: &Vec<Vector>) -> Vec<Vector> {
-        let dirs: Vec<Vector> = vec![
-            Vector { x: 1, y: 0, z: 0 },
-            Vector { x: 0, y: 1, z: 0 },
-            Vector { x: 0, y: 0, z: 1 },
-        ];
-        let mut surrounding_cubes: Vec<Vector> = Vec::new();
-
-        for dir in &dirs {
-            if cubes.contains(&self.add(dir)) {
-                surrounding_cubes.push(self.add(dir));
-            }
-            if cubes.contains(&self.add(&dir.opposite())) {
-                surrounding_cubes.push(self.add(&dir.opposite()));
-            }
-        }
-
-        surrounding_cubes
-    }
-}
+use std::collections::{HashSet, VecDeque};
 
 pub fn run(input: String) -> (usize, usize) {
-    let cubes: Vec<Vector> = input
+    let cubes: Vec<Vec<i32>> = input
         .lines()
-        .map(|x| {
-            let mut x = x.split(',');
-            Vector {
-                x: x.next().unwrap().parse().unwrap(),
-                y: x.next().unwrap().parse().unwrap(),
-                z: x.next().unwrap().parse().unwrap(),
-            }
-        })
+        .map(|x| x.split(',').map(|y| y.parse().unwrap()).collect())
         .collect();
 
     let answer_one = part_one(&cubes);
@@ -64,38 +12,63 @@ pub fn run(input: String) -> (usize, usize) {
     (answer_one, answer_two)
 }
 
-fn part_one(cubes: &Vec<Vector>) -> usize {
+fn part_one(cubes: &Vec<Vec<i32>>) -> usize {
     let mut surface_area = 0;
     for cube in cubes {
-        surface_area += 6 - cube.check_surrounding(cubes).len();
+        let mut open_surface = 6;
+        for i in 0..3 {
+            for j in [-1, 1] {
+                let mut adjacent_cube = cube.clone();
+                adjacent_cube[i] += j;
+                if cubes.contains(&adjacent_cube) {
+                    open_surface -= 1;
+                }
+            }
+        }
+        surface_area += open_surface;
     }
 
     surface_area
 }
 
-fn part_two(cubes: &Vec<Vector>) -> usize {
-    let dirs: Vec<Vector> = vec![
-        Vector { x: 1, y: 0, z: 0 },
-        Vector { x: 0, y: 1, z: 0 },
-        Vector { x: 0, y: 0, z: 1 },
-    ];
-    let mut air_cubes: HashSet<Vector> = HashSet::new();
+fn part_two(cubes: &Vec<Vec<i32>>) -> usize {
+    let mut air_cubes: HashSet<Vec<i32>> = HashSet::new();
+    let mut frontier: VecDeque<Vec<i32>> = VecDeque::new();
+    let mut surface_area = 0;
 
-    'outer: for cube in cubes {
-        for dir in &dirs {
-            if !cubes.contains(&cube.add(dir)) {
-                if cube.add(dir).check_surrounding(cubes).len() == 6 {
-                    air_cubes.insert(cube.add(dir));
-                }
-            }
-            if !cubes.contains(&cube.add(&dir.opposite())) {
-                if cube.add(&dir.opposite()).check_surrounding(cubes).len() == 6 {
-                    air_cubes.insert(cube.add(&dir.opposite()));
+    let x_min = cubes.iter().map(|x| x[0]).min().unwrap() - 1;
+    let y_min = cubes.iter().map(|x| x[1]).min().unwrap() - 1;
+    let z_min = cubes.iter().map(|x| x[2]).min().unwrap() - 1;
+    let x_max = cubes.iter().map(|x| x[0]).max().unwrap() + 1;
+    let y_max = cubes.iter().map(|x| x[1]).max().unwrap() + 1;
+    let z_max = cubes.iter().map(|x| x[2]).max().unwrap() + 1;
+
+    for z in z_min..=z_max {
+        for x in x_min..=x_max {
+            frontier.push_back(vec![x, y_min, z]);
+            air_cubes.insert(vec![x, y_min, z]);
+        }
+    }
+
+    while !frontier.is_empty() {
+        let cube = frontier.pop_front().unwrap();
+        for i in 0..3 {
+            for j in [-1, 1] {
+                let mut adjacent_cube = cube.clone();
+                adjacent_cube[i] += j;
+                if (x_min..=x_max).contains(&adjacent_cube[0])
+                    && (y_min..=y_max).contains(&adjacent_cube[1])
+                    && (z_min..=z_max).contains(&adjacent_cube[2])
+                {
+                    if cubes.contains(&adjacent_cube) {
+                        surface_area += 1;
+                    } else if air_cubes.insert(adjacent_cube.clone()) {
+                        frontier.push_back(adjacent_cube);
+                    }
                 }
             }
         }
     }
 
-    println!("{:?}", air_cubes);
-    part_one(cubes) - (air_cubes.len() * 6)
+    surface_area
 }
