70365ff2e39483117c070b8b0930ae1b9c9853f2
hesamzkr <hesam.zk.81@gmail.com>
2022-12-24 19:23:06 +0100

    add year title to macro

--- src/common.rs
+++ src/common.rs
@@ -5,15 +5,16 @@ macro_rules! solve {
         let year = $year;
         let day = &stringify!($md)[3..];
         let day_title = format!("--------- Day {day} ---------");
-        println!("\n{}", day_title);
+        println!("\n   Advent Of Code {year}");
+        println!("{day_title}");
         match fs::read_to_string(format!("inputs/year{year}/input_{day}.txt")) {
             Ok(input_str) => {
                 let start_time = std::time::Instant::now();
                 let (answer_one, answer_two) = $md::run(input_str);
                 let end_time = std::time::Instant::now();
-                println!("Part One: {}", answer_one);
+                println!("Part One: {answer_one}");
 
-                println!("Part Two: {}", answer_two);
+                println!("Part Two: {answer_two}");
 
                 println!("{}", "-".repeat(day_title.len()));
                 println!("Time taken: {:.2?}", end_time - start_time);
