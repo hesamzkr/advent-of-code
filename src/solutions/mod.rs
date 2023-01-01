6ed2effb3f956cfbd838b82c33dd8257ffca68bc
hesamzkr <hesam.zk.81@gmail.com>
2023-01-01 19:41:34 +0100

    setup for 2021

--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -1,7 +1,9 @@
+mod year2021;
 mod year2022;
 
 pub fn run(year: u16, day: u16) {
     match year {
+        2021 => year2021::run(day),
         2022 => year2022::run(day),
 
         2015..=2022 => println!("Year {year} is not implemented"),
