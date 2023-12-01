15cf1cabc27ed7a4ae33b3a8f03ea71fe9362eac
Hesam Zakeri <hesam.zk.81@gmail.com>
2023-12-01 10:18:34 +0100

    2023_1 initial solution

--- 2021-23/src/solutions/year2023/day1.rs
+++ 2021-23/src/solutions/year2023/day1.rs
@@ -8,9 +8,51 @@ pub fn run(input: String) -> (u32, u32) {
 }
 
 fn part_one(input: &[&str]) -> u32 {
-    0
+    let mut sum = 0;
+    for line in input {
+        let mut numbers = vec![];
+        for char in line.chars() {
+            if char.is_numeric() {
+                numbers.push(char);
+            }
+        }
+
+        sum += format!("{}{}", numbers[0], numbers[numbers.len() - 1])
+            .parse::<u32>()
+            .unwrap();
+    }
+
+    sum
 }
 
 fn part_two(input: &[&str]) -> u32 {
-    0
+    let mut sum = 0;
+    let word_numbers: [&str; 9] = [
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+    for line in input {
+        let mut numbers: Vec<(String, usize)> = vec![];
+
+        for (i, char) in line.chars().enumerate() {
+            if char.is_numeric() {
+                numbers.push((char.to_string(), i));
+            }
+        }
+
+        for (i, word) in word_numbers.iter().enumerate() {
+            if let indices = line.match_indices(word).collect::<Vec<_>>() {
+                for index in indices {
+                    numbers.push((format!("{}", i + 1), index.0));
+                }
+            }
+        }
+
+        numbers.sort_by(|a, b| a.1.cmp(&b.1));
+
+        sum += format!("{}{}", numbers[0].0, numbers[numbers.len() - 1].0)
+            .parse::<u32>()
+            .unwrap();
+    }
+
+    sum
 }
