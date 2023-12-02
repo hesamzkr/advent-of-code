caa0d40aea2b71123009ffa7bb80b235f63328b3
hesamzkr <hesamaldinzakeri@gmail.com>
2023-12-02 15:13:24 +0100

    prep y2023d3 fixed

--- 2021-23/src/solutions/year2023/day3.rs
+++ 2021-23/src/solutions/year2023/day3.rs
@@ -14,22 +14,3 @@ fn part_one(input: &[&str]) -> u32 {
 fn part_two(input: &[&str]) -> u32 {
     0
 }
-
-#[macro_export]
-macro_rules! safe_unpack {
-    ($list:tt, ()) => { };
-    ($list:tt, $name:tt) => {
-        let $name = match $list.next() {
-            Some(val) => val,
-            None => return Err(String::from("Not enough values in list.")),
-        };
-    };
-    ($list:tt, $name:tt $(, $vars:tt)+) => {
-        safe_unpack!($list, $name);
-        safe_unpack!($list $(, $vars)+)
-    };
-    ($e:expr $(, $vars:tt)+) => {
-        let mut temp_iter = $e;
-        safe_unpack!(temp_iter $(, $vars)+);
-    };
-}
