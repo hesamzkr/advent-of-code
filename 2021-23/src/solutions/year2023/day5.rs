1159270f38b4f33586a614784c8b406431a37778
Hesam Zakeri <hesam.zk.81@gmail.com>
2023-12-07 10:07:04 +0100

    y2023d5 initial first part

--- 2021-23/src/solutions/year2023/day5.rs
+++ 2021-23/src/solutions/year2023/day5.rs
@@ -1,16 +1,64 @@
-pub fn run(input: String) -> (u32, u32) {
-    let input: Vec<&str> = input.lines().collect();
+use std::convert;
 
-    let answer_one = part_one(&input);
-    let answer_two = part_two(&input);
+#[derive(Debug)]
+struct Convert {
+    destination: u32,
+    source: u32,
+    length: u32,
+}
+
+pub fn run(input: String) -> (u32, u32) {
+    let input: Vec<&str> = input.split("\n\n").collect();
+
+    let seeds = input[0].split(':').collect::<Vec<&str>>()[1]
+        .split_whitespace()
+        .map(|num| num.parse::<u32>().unwrap())
+        .collect::<Vec<u32>>();
+
+    let mut parsed: Vec<Vec<Convert>> = vec![];
+    for i in 1..input.len() {
+        let converts: Vec<Convert> = input[i]
+            .lines()
+            .skip(1)
+            .map(|line| {
+                let numbers: Vec<u32> = line
+                    .split_whitespace()
+                    .map(|num| num.parse::<u32>().unwrap())
+                    .collect();
+
+                Convert {
+                    destination: numbers[0],
+                    source: numbers[1],
+                    length: numbers[2],
+                }
+            })
+            .collect();
+
+        parsed.push(converts);
+    }
+
+    let answer_one = part_one(&seeds, &parsed);
+    let answer_two = part_two(&parsed);
 
     (answer_one, answer_two)
 }
 
-fn part_one(input: &[&str]) -> u32 {
+fn part_one(seeds: &Vec<u32>, maps: &Vec<Vec<Convert>>) -> u32 {
+    for seed in seeds {
+        let mut last = *seed;
+        for map in maps {
+            for convert in map {
+                if last >= convert.source
+                    && last <= convert.source + convert.length + convert.length - 1
+                {
+                }
+            }
+        }
+    }
+
     0
 }
 
-fn part_two(input: &[&str]) -> u32 {
+fn part_two(input: &Vec<Vec<Convert>>) -> u32 {
     0
 }
