368e2919113b786fa4164e402826cd129dceafeb
Hesam Zakeri <hesam.zk.81@gmail.com>
2023-12-07 10:06:47 +0100

    Update mod.rs

--- 2021-23/src/solutions/year2023/mod.rs
+++ 2021-23/src/solutions/year2023/mod.rs
@@ -1,11 +1,11 @@
 use crate::{generate_year_match, import_modules};
 
-import_modules!(day1, day2, day3, day4, day5);
+import_modules!(day1, day2, day3, day4, day5, day6, day7);
 
 const YEAR: u16 = 2023;
 
 pub fn run(day: u16) {
     let day_string = format!("day{day}");
     let day_str = day_string.as_str();
-    generate_year_match!(day_str, day1, day2, day3, day4, day5);
+    generate_year_match!(day_str, day1, day2, day3, day4, day5, day6, day7);
 }
